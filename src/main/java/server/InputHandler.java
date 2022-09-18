f0c561948cd649c418b00762172d11d238fca8c3
KaitoKunTatsu <kaitokuntatsu@gmail.com>
2022-09-18 15:56:19 +0200

    comments

--- src/main/java/server/InputHandler.java
+++ src/main/java/server/InputHandler.java
@@ -18,10 +18,12 @@ import java.util.HashMap;
 import java.util.List;
 
 /**
- * This class acts as a backend for the KMes Server and processes all inputs from user sockets.
+ * this class processes the requests of a client socket in a new thread
  *
  * @version v3.0.0 | last edit: 16.09.2022
  * @author Joshua H. | KaitoKunTatsu#3656
+ *
+ * @see SocketWrapper
  * */
 class InputHandler extends Thread {
 
@@ -34,125 +36,31 @@ class InputHandler extends Thread {
     private boolean running;
 
     /**
+     * Creates an instance of this class, initializes utility classes like {@link SQLUtils} and connets the database
      *
-     * @param pAllConnectedClients
+     * @param pClient               {@link SocketWrapper} with the client socket whose request will be processed
+     * @param pAllConnectedClients  {@link HashMap} containing all users connected to the server and linked to their username
+     * @param pQueuedMessages       {@link HashMap} containing all messages which couldn't be sent due to user inactivity
      *
-     * @param pClient               {@link SocketWrapper}
-     *
-     * @see EncryptionUtils
-     * @see SQLUtils
+     * @see SocketAcceptor
      * */
-    protected InputHandler(SocketWrapper pClient, HashMap<String, SocketWrapper> pAllConnectedClients, HashMap<String, List<String>> pQueuedMessages) throws SQLException {
+    public InputHandler(SocketWrapper pClient, HashMap<String, SocketWrapper> pAllConnectedClients, HashMap<String, List<String>> pQueuedMessages) throws SQLException {
         this.allConnectedClients = pAllConnectedClients;
         this.queuedMessages = pQueuedMessages;
         this.sqlUtils = new SQLUtils("src/main/resources/kmes_server.db");
         this.client = pClient;
     }
 
-    private SocketWrapper getWrapper(String pUsername) { return allConnectedClients.get(pUsername); }
-
-    private void handleSendRequest(String pReveiver, String pMessage) throws InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException, IOException, InvalidKeyException {
-        System.err.println(pReveiver);
-        if (!userExists(pReveiver))
-            client.writeAES("error;;User does not exist;;Couldn't send message");
-        else if (pReveiver.equals(currentUser))
-            client.writeAES("error;;You can't text yourself;;Couldn't send message");
-        else if (currentUser.equals(""))
-            client.writeAES("error;;You have to be logged in before sending messages;;Couldn't send message");
-        else
-        {
-            SocketWrapper lReceiverWrapper = getWrapper(pReveiver);
-            if (lReceiverWrapper == null) queueMessage(pReveiver, pMessage);
-            else
-            {
-                lReceiverWrapper.writeAES("message;;"+
-                        currentUser+";;"+pMessage);
-            }
-        }
-    }
-
-    private void queueMessage(String pReveiver, String pMessage)
-    {
-        queuedMessages.putIfAbsent(pReveiver, new ArrayList<>());
-        queuedMessages.get(pReveiver).add(currentUser+";;"+pMessage);
-        System.out.println(queuedMessages.get(pReveiver).size());
-    }
-
-    private void sendQueuedMessages() throws InvalidAlgorithmParameterException, IllegalBlockSizeException, IOException, BadPaddingException, InvalidKeyException {
-        List<String> lMessages = queuedMessages.get(currentUser);
-        if (lMessages == null) return;
-        while (lMessages.size() != 0) {
-            System.out.println(lMessages.size());
-            client.writeAES("message;;"+lMessages.get(0));
-            lMessages.remove(0);
-        }
-    }
-
-    private void changeBindingWithCurrentUser()
-    {
-        SocketWrapper lCurrentBindedWrapperToThisUsername = allConnectedClients.get(currentUser);
-        if (lCurrentBindedWrapperToThisUsername != null) {
-            try {
-                lCurrentBindedWrapperToThisUsername.writeAES("loggedOut");
-            }
-            catch (InvalidAlgorithmParameterException | IOException | IllegalBlockSizeException | BadPaddingException | InvalidKeyException ignored) {}
-        }
-        allConnectedClients.put(currentUser, client);
-    }
-
-    private void handleRegistrationRequest(String pUsername, String pPassword) throws IOException, SQLException, IllegalBlockSizeException, BadPaddingException, InvalidKeyException, InvalidAlgorithmParameterException {
-        if (!currentUser.equals("")) {
-            client.writeAES("error;;Log out before registration;;Registration failed");
-        }
-        else
-        {
-            if (!userExists(pUsername))
-            {
-                try
-                {
-                    byte[] lSalt = EncryptionUtils.generateSalt();
-                    String lHashedPassword = EncryptionUtils.getHash(pPassword, lSalt);
-                    sqlUtils.onExecute("INSERT INTO User VALUES(?, ?, ?)", pUsername, lHashedPassword, lSalt);
-                    client.writeAES("loggedIn;;"+pUsername);
-                    changeBindingWithCurrentUser();
-                    sendQueuedMessages();
-                }
-                catch (NoSuchAlgorithmException | InvalidKeySpecException ex) {
-                    client.writeAES("error;;Couldn't encrypt your password;;Error occurred during registration process");
-                }
-            }
-            else
-            {
-                client.writeAES("error;;This username is already taken, please choose another one;;Registration failed");
-            }
-        }
-    }
-
-    private void handleLoginRequest(String pUsername, String pPassword) throws IOException, IllegalBlockSizeException, BadPaddingException, InvalidKeyException, InvalidAlgorithmParameterException {
-        byte[] lSalt;
-        try {
-            lSalt = sqlUtils.onQuery("SELECT salt FROM User WHERE username=?", pUsername).getBytes(1);
-        } catch (SQLException sqlEx) {
-            client.writeAES("error;;User not found;;Login failed");
-            return;
-        }
-
-        if (verifyLogin(pUsername, pPassword, lSalt))
-        {
-            currentUser = pUsername;
-            client.writeAES("loggedIn;;"+pUsername);
-            changeBindingWithCurrentUser();
-            sendQueuedMessages();
-        }
-        else {
-            client.writeAES("error;;Password or username incorrect;;Login failed");
-        }
-    }
-
-
     /**
-     *
+     *  Reads and processes client inputs like:<br>
+     *  - login<br>
+     *  - registration<br>
+     *  - logout<br>
+     *  - send message<br>
+     *  - check other user's existance<br>
+     *  requests
      * */
+    @Override
     public void run() {
         running = true;
         while (running)
@@ -207,8 +115,93 @@ class InputHandler extends Thread {
         }
     }
 
+    private void handleSendRequest(String pReveiver, String pMessage) throws InvalidAlgorithmParameterException, IllegalBlockSizeException, BadPaddingException, IOException, InvalidKeyException {
+        System.err.println(pReveiver);
+        if (!userExists(pReveiver))
+            client.writeAES("error;;User does not exist;;Couldn't send message");
+        else if (pReveiver.equals(currentUser))
+            client.writeAES("error;;You can't text yourself;;Couldn't send message");
+        else if (currentUser.equals(""))
+            client.writeAES("error;;You have to be logged in before sending messages;;Couldn't send message");
+        else
+        {
+            SocketWrapper lReceiverWrapper = getWrapper(pReveiver);
+            if (lReceiverWrapper == null) queueMessage(pReveiver, pMessage);
+            else
+            {
+                lReceiverWrapper.writeAES("message;;"+
+                        currentUser+";;"+pMessage);
+            }
+        }
+    }
 
-    public boolean verifyLogin(String pUsername, String pPassword, byte[] pSalt)
+    private void handleRegistrationRequest(String pUsername, String pPassword) throws IOException, SQLException, IllegalBlockSizeException, BadPaddingException, InvalidKeyException, InvalidAlgorithmParameterException {
+        if (!currentUser.equals("")) {
+            client.writeAES("error;;Log out before registration;;Registration failed");
+        }
+        else
+        {
+            if (!userExists(pUsername))
+            {
+                try
+                {
+                    byte[] lSalt = EncryptionUtils.generateSalt();
+                    String lHashedPassword = EncryptionUtils.getHash(pPassword, lSalt);
+                    sqlUtils.onExecute("INSERT INTO User VALUES(?, ?, ?)", pUsername, lHashedPassword, lSalt);
+                    client.writeAES("loggedIn;;"+pUsername);
+                    changeBindingWithCurrentUser();
+                    sendQueuedMessages();
+                }
+                catch (NoSuchAlgorithmException | InvalidKeySpecException ex) {
+                    client.writeAES("error;;Couldn't encrypt your password;;Error occurred during registration process");
+                }
+            }
+            else
+            {
+                client.writeAES("error;;This username is already taken, please choose another one;;Registration failed");
+            }
+        }
+    }
+
+    private void handleLoginRequest(String pUsername, String pPassword) throws IOException, IllegalBlockSizeException, BadPaddingException, InvalidKeyException, InvalidAlgorithmParameterException {
+        byte[] lSalt;
+        try {
+            lSalt = sqlUtils.onQuery("SELECT salt FROM User WHERE username=?", pUsername).getBytes(1);
+        } catch (SQLException sqlEx) {
+            client.writeAES("error;;User not found;;Login failed");
+            return;
+        }
+
+        if (verifyLogin(pUsername, pPassword, lSalt))
+        {
+            currentUser = pUsername;
+            client.writeAES("loggedIn;;"+pUsername);
+            changeBindingWithCurrentUser();
+            sendQueuedMessages();
+        }
+        else {
+            client.writeAES("error;;Password or username incorrect;;Login failed");
+        }
+    }
+
+    private void queueMessage(String pReveiver, String pMessage)
+    {
+        queuedMessages.putIfAbsent(pReveiver, new ArrayList<>());
+        queuedMessages.get(pReveiver).add(currentUser+";;"+pMessage);
+        System.out.println(queuedMessages.get(pReveiver).size());
+    }
+
+    private void sendQueuedMessages() throws InvalidAlgorithmParameterException, IllegalBlockSizeException, IOException, BadPaddingException, InvalidKeyException {
+        List<String> lMessages = queuedMessages.get(currentUser);
+        if (lMessages == null) return;
+        while (lMessages.size() != 0) {
+            System.out.println(lMessages.size());
+            client.writeAES("message;;"+lMessages.get(0));
+            lMessages.remove(0);
+        }
+    }
+
+    private boolean verifyLogin(String pUsername, String pPassword, byte[] pSalt)
     {
         try
         {
@@ -226,7 +219,7 @@ class InputHandler extends Thread {
         }
     }
 
-    public boolean userExists(String pUsername)
+    private boolean userExists(String pUsername)
     {
         try {
             ResultSet lResultSet = sqlUtils.onQuery("SELECT username FROM User WHERE username=?", pUsername);
@@ -237,6 +230,20 @@ class InputHandler extends Thread {
             return false;
         }
     }
+
+    private void changeBindingWithCurrentUser()
+    {
+        SocketWrapper lCurrentBindedWrapperToThisUsername = allConnectedClients.get(currentUser);
+        if (lCurrentBindedWrapperToThisUsername != null) {
+            try {
+                lCurrentBindedWrapperToThisUsername.writeAES("loggedOut");
+            }
+            catch (InvalidAlgorithmParameterException | IOException | IllegalBlockSizeException | BadPaddingException | InvalidKeyException ignored) {}
+        }
+        allConnectedClients.put(currentUser, client);
+    }
+
+    private SocketWrapper getWrapper(String pUsername) { return allConnectedClients.get(pUsername); }
 
     public void stopListeningForInput() {running = false;}
 }
